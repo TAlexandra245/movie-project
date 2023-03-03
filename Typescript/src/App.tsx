7915e9de699d11d4a6016d922c8e32d7b04c21cd
Toma <tomaalexandra8@gmail.com>
2023-03-03 23:19:47 +0200

    Implemented cards

--- Typescript/src/App.tsx
+++ Typescript/src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+
 import './App.css'
 import Movies from './components/Movies'
+
+
+
 
 function App() {
 
   return (
-
-    <div className="App">
-     <h1>Movie project</h1>
-     <Movies>
-     </Movies>
-    </div>
+      <div className="App">
+        <h1>Movie project</h1>
+        <Movies />
+      </div>
   )
 }
 
--- Typescript/src/components/Movies.tsx
+++ Typescript/src/components/Movies.tsx
@@ -1,9 +1,15 @@
 import MovieProps from "../MovieSinglePost";
 import useMovies from "../hooks/useMovies";
-import MoviePost from "./MoviePost";
-import Pagination from "./Pagination";
 import React from 'react'
-import {useState, useEffect} from 'react'
+import { useState} from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styled from 'styled-components'
+import { Pagination } from "antd";
 
 const Movies = (props: { posts: MovieProps[] }) => {
 
@@ -11,21 +17,59 @@ const Movies = (props: { posts: MovieProps[] }) => {
 
     //pagination hooks
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(150);
+    const [postsPerPage, setPostsPerPage] = useState(9);
 
     const lastPostIndex = currentPage * postsPerPage;
     const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentMovies = movies?.slice(firstPostIndex, lastPostIndex)
+    const currentMovies = movies?.slice(firstPostIndex, lastPostIndex);
+
+    const onShowSizeChange = (current:any, pageSize:any) => {
+        setPostsPerPage(pageSize);
+    }
+    
+    const Container = styled.div`
+    display: flex;
+    gap: 2em;
+    justify-content:center;
+    flex-wrap: wrap;
+    margin-bottom: 2em;
+    `
 
     return (
-        <div className="card-users">
-            {
-                currentMovies?.map((movie) => 
-                <MoviePost info ={movie.info} title ={movie.title} year ={movie.year}></MoviePost>
-                )
-            }
-            <Pagination totalPosts={movies?.length} postsPerPage={postsPerPage} setCurrentPage ={setCurrentPage}></Pagination>
-
+        <div>
+            <Container>
+                {
+                    currentMovies?.map((movie) =>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                sx={{ height: 140 }}
+                                image={movie.info.image_url}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {movie.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {movie.info.plot}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">{movie.year}</Button>
+                                <Button size="small">{movie.info.rating}</Button>
+                            </CardActions>
+                        </Card>
+                    )
+                }
+            </Container>
+            <Pagination
+            onChange={(value:any) => setCurrentPage(value)}
+            pageSize = {postsPerPage}
+            total = {movies?.length}
+            current ={currentPage}
+            showSizeChanger
+            showQuickJumper
+            onShowSizeChange={onShowSizeChange}
+            />
         </div>
     );
 }
